9ef6c647faf2afcdd39c765e7b401cc4c1d9fa24
Amanda Ramos <103595539+amandaramos-warren@users.noreply.github.com>
2022-07-18 17:47:13 -0300

    refactor: register errorMiddleware

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import './di/index';
 import express from 'express';
 import UserRoutes from './presentation/Routes';
 import { container } from 'tsyringe';
+import errorMiddleware from './middlewares/errorMiddleware';
 
 const app = express();
 
 app.use(express.json());
 app.use(container.resolve(UserRoutes).router);
+app.use(errorMiddleware);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000, localhost:3000');
