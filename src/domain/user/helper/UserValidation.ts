61e45ea9fd94a66a78a9661d6d4953fc9b454d94
Amanda Ramos <103595539+amandaramos-warren@users.noreply.github.com>
2022-07-19 16:19:24 -0300

    refactor:  implemented IUserValidation

--- src/domain/user/helper/UserValidation.ts
+++ src/domain/user/helper/UserValidation.ts
@@ -4,7 +4,7 @@ import IUserHelper from '../../../interfaces/domain/IUserHelper';
 import IUserValidation from '../../../interfaces/domain/IUserValidation';
 
 @injectable()
-export default class UserValidation {
+export default class UserValidation implements IUserValidation {
   userHelper: IUserHelper;
   constructor(@inject('UserHelper') userHelper: IUserHelper) {
     this.userHelper = userHelper;
@@ -12,7 +12,7 @@ export default class UserValidation {
 
   validate(cpf: string, email: string, database: IUser[]) {
     this.userHelper.cpfValidate(cpf);
-    this.userHelper.checkIfEquals(email, 'email', database);
     this.userHelper.checkIfEquals(cpf, 'cpf', database);
+    this.userHelper.checkIfEquals(email, 'email', database);
   }
 }
