afbb8a1e51980ffc7ef415b04cfd73707b398604
Amanda Ramos <103595539+amandaramos-warren@users.noreply.github.com>
2022-07-20 16:59:51 -0300

    feat: replaced UserController interfaces with IController

--- src/interfaces/presentation/IController.ts
+++ src/interfaces/presentation/IController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 
-export default interface IUserListController {
+export default interface IController {
   handle(req: Request, res: Response, next: NextFunction): void;
 }
